a7cced21ca6b3384958e2c20fe5e4571465c0af8
akramakh <akram.isocial@gmail.com>
2019-07-30 14:01:28 +0300

    initial commit

---
+++ blogs/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Blog
+# Create your views here.
+
+def index(request):
+    blogs = Blog.objects.all()
+    context = {'blogs':blogs}
+    return render(request, 'blogs/index.html', context)
